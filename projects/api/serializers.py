3156a2f8f9007885cfea55a213a03daa486cd3a6
zackckpetersen <zackcpetersen@gmail.com>
2020-11-11 21:26:23 -0700

    creating projects app, api, and tests

--- projects/api/serializers.py
+++ projects/api/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from projects.models import Project, ProjectImage
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = '__all__'
-
-
 class ProjectImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectImage
+        fields = '__all__'
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    project_images = ProjectImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
         fields = '__all__'
--- projects/models.py
+++ projects/models.py
@@ -3,12 +3,16 @@ from django.db import models
 
 class Project(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     # type = models.ForeignKey() ??
     # status = models.CharField(choices=) ??
 
 
 class ProjectImage(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=255, blank=True, null=True)
     project = models.ForeignKey(Project, on_delete=models.PROTECT, related_name='project_images')
     image = models.ImageField(upload_to='images')
     created_at = models.DateTimeField(auto_now_add=True)
