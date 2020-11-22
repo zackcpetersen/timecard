b66a36aa19cd5b5b28ff2841eba4ba68a864856a
zackckpetersen <zackcpetersen@gmail.com>
2020-11-22 12:22:37 -0700

    adding ebs stuff

--- accounts/management/commands/createsu.py
+++ accounts/management/commands/createsu.py
@@ -1,9 +1,13 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from accounts.models import User
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("zack@gmail.com", password="admin")
+        if not User.objects.filter(email="zack@gmail.com").exists():
+            User.objects.create_superuser(email="zack@gmail.com",
+                                          password="admin",
+                                          first_name='Zack',
+                                          last_name='Petersen')
+            self.stdout.write(self.style.SUCCESS('Successfully created superuser'))
