7c786db265d562135be816d789d4dc41c5b99488
zackckpetersen <zackcpetersen@gmail.com>
2020-11-22 20:40:37 -0700

    updating createsu management command

--- accounts/management/commands/createsu.py
+++ accounts/management/commands/createsu.py
@@ -8,8 +8,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(email="zack@gmail.com").exists():
+            password = os.environ.get('SUPERUSER_PASS') if os.environ.get('SUPERUSER_PASS') else 'admin'
             User.objects.create_superuser(email="zackcpetersen@gmail.com",
-                                          password=os.environ['SUPERUSER_PASS'],
+                                          password=password,
                                           first_name='Zack',
                                           last_name='Petersen')
             self.stdout.write(self.style.SUCCESS('Successfully created superuser'))
