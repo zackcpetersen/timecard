38b92b98e8798611a02b69d7dd1cde3eb8824263
zackckpetersen <zackcpetersen@gmail.com>
2020-11-24 12:13:51 -0700

    updating settings for elastic beanstalk

--- projects/models.py
+++ projects/models.py
@@ -17,5 +17,5 @@ class ProjectImage(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=255, blank=True, null=True)
     project = models.ForeignKey(Project, on_delete=models.PROTECT, related_name='project_images')
-    image = models.ImageField(upload_to='images')
+    image = models.ImageField(upload_to='project-images')
     created_at = models.DateTimeField(auto_now_add=True)
