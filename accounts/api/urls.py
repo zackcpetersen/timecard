61770de03726c1f9a1eae26fbcddae376c50a714
zackckpetersen <zackcpetersen@gmail.com>
2021-02-06 20:36:28 -0700

    removing commented out path

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -7,8 +7,6 @@ router = routers.DefaultRouter()
 
 router.register(r'users', account_views.UserCreationViewSet)
 router.register(r'update-user', account_views.UserUpdateViewSet)
-# router.register(r'current-user', account_views.CurrentUserGetViewSet)
-
 
 urlpatterns = [
     path('accounts/', include('rest_auth.urls')),
