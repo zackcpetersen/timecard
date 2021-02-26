776cede9060387a288c91072827c7286d8bfde2c
zackckpetersen <zackcpetersen@gmail.com>
2021-02-26 15:54:55 -0700

    updating allowed hosts

--- timecard/settings/base.py
+++ timecard/settings/base.py
@@ -25,7 +25,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['timecard-dev.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['staging-timecard-dev.us-west-2.elasticbeanstalk.com']
 
 # Auth User Model
 AUTH_USER_MODEL = 'accounts.User'
